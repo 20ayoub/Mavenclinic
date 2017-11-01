5817fbb13553c668f6dddb2b0be8c78d47a82fbf
20ayoub <33100768+20ayoub@users.noreply.github.com>
2017-11-01 20:13:15 +0000

    Update ValidatorTests.java

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -26,12 +26,20 @@ public class ValidatorTests {
     }
 
     @Test
-    public voite(person);
+    public void shouldNotValidateWhenFirstNameEmpty() {
+
+        LocaleContextHolder.setLocale(Locale.ENGLISH);
+        Person person = new Person();
+        person.setFirstName("");
+        person.setLastName("smith");
+
+        Validator validator = createValidator();
+        Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
 
         assertThat(constraintViolations.size()).isEqualTo(1);
         ConstraintViolation<Person> violation = constraintViolations.iterator().next();
         assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
-        assertThat(violation.getMessage()).isEqualTo("may not be emptye");
+        assertThat(violation.getMessage()).isEqualTo("may not be empty");
         
     }
 
